9f9cc33d41c11a8a11e6b166c2ca02c77469bbbb
Alex Lee <hyunho98@gmail.com>
2019-10-28 07:23:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,12 +83,12 @@ def adding_matz
         }
      }
 
-		{programmer_hash.to_a.unshift(:yukihiro_matsumoto => {
+		Hash[programmer_hash.to_a.unshift(:yukihiro_matsumoto => {
 				:known_for => "Ruby",
 				:languages => ["LISP", "C"]
 			}
 			)
-		}
+		]
 end
 
 def changing_alan
